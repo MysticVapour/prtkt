27ab282fe65c61f29b6eec4185a51c61f5359d88
grovertanya <106971654+grovertanya@users.noreply.github.com>
2024-09-09 00:03:53 -0400

    login page + timetable page

--- frontend/prtkt/prtkt/ContentView.swift
+++ frontend/prtkt/prtkt/ContentView.swift
@@ -8,17 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername: Float = 0
+    @State private var wrongPassword: Float = 0
+    @State private var showingLoginScreen = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                     Color(red: 0.7, green: 0.9, blue: 0.7)
+                    .ignoresSafeArea()
+                
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(Color.red, width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(Color.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background( Color(red: 0.8, green: 0.9, blue: 0.8 ))
+                    .cornerRadius(10)
+                    
+                    NavigationLink(
+                        destination: Text("You are logged in @\(username)"),
+                        isActive: $showingLoginScreen
+                    ) {
+                        EmptyView() // This is needed to satisfy the requirement of NavigationLink
+                    }
+                }
+            }
+            .navigationBarHidden(true)
         }
-        .padding()
+    }
+
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "mario2021" {
+            wrongUsername = 0
+            if password.lowercased() == "abc123" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
---
+++ frontend/prtkt/prtkt/timetable.swift
@@ -0,0 +1,9 @@
+//
+//  timetable.swift
+//  prtkt
+//
+//  Created by Tanya Grover on 9/7/24.
+//
+
+import Foundation
+
